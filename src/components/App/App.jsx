b0335cef1a102920b80fcf49bd9f16e25e77668e
Daniil Surin <wkingsurin@gmail.com>
2025-03-24 14:54:33 +0500

    feat: Extract JSX of 'modalSettings' into a separate component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -357,7 +357,8 @@ export default function App() {
     const targetSvg = targetBtn.querySelector("svg");
 
     const coordinates = targetSvg.getBoundingClientRect();
-    const position = { x: coordinates.x - 30, y: coordinates.y + 30 };
+    console.log(`coordinates`, coordinates);
+    const position = { x: coordinates.right - 254, y: coordinates.bottom + 30 };
 
     content.newTask.timeModal.isOpen
       ? closeTimeModal()
@@ -531,123 +532,16 @@ export default function App() {
           <ModalNotification newTask={content.newTask}></ModalNotification>
         )}
         {content.newTask.timeModal.isOpen && (
-          <ModalSettings data={content.newTask.timeModal}></ModalSettings>
+          <ModalSettings
+            task={content.newTask}
+            Chevron={Chevron}
+          ></ModalSettings>
         )}
         {/* <div className="modal notification">
           <div className="content">
             <p className="text">{notificationText}</p>
           </div>
         </div> */}
-        <div
-          className="modal settings extended-modal"
-          style={{ border: "1px solid #e2e2e2" }}
-        >
-          <div className="content">
-            <div className="block calendar">
-              <div className="period">
-                <button className="btn">
-                  <Chevron rotate={-90}></Chevron>
-                </button>
-                <p className="text">Март</p>
-                <button className="btn">
-                  <Chevron rotate={90}></Chevron>
-                </button>
-              </div>
-              <div className="period">
-                <button className="btn">
-                  <Chevron rotate={-90}></Chevron>
-                </button>
-                <p className="text">2025</p>
-                <button className="btn">
-                  <Chevron rotate={90}></Chevron>
-                </button>
-              </div>
-              <table className="calendar-table">
-                <tbody>
-                  <tr className="table-row">
-                    <th className="table-heading">Пн</th>
-                    <th className="table-heading">Вт</th>
-                    <th className="table-heading">Ср</th>
-                    <th className="table-heading">Чт</th>
-                    <th className="table-heading">Пт</th>
-                    <th className="table-heading">Сб</th>
-                    <th className="table-heading">Вс</th>
-                  </tr>
-                  <tr className="table-row">
-                    <td className="table-data">1</td>
-                    <td className="table-data">2</td>
-                    <td className="table-data">3</td>
-                    <td className="table-data">4</td>
-                    <td className="table-data">5</td>
-                    <td className="table-data">6</td>
-                    <td className="table-data">7</td>
-                  </tr>
-                  <tr className="table-row">
-                    <td className="table-data">8</td>
-                    <td className="table-data">9</td>
-                    <td className="table-data">10</td>
-                    <td className="table-data">11</td>
-                    <td className="table-data">12</td>
-                    <td className="table-data">13</td>
-                    <td className="table-data">14</td>
-                  </tr>
-                  <tr className="table-row">
-                    <td className="table-data">15</td>
-                    <td className="table-data">16</td>
-                    <td className="table-data">17</td>
-                    <td className="table-data">18</td>
-                    <td className="table-data">19</td>
-                    <td className="table-data">20</td>
-                    <td className="table-data">21</td>
-                  </tr>
-                  <tr className="table-row">
-                    <td className="table-data">22</td>
-                    <td className="table-data">23</td>
-                    <td className="table-data">24</td>
-                    <td className="table-data">25</td>
-                    <td className="table-data">26</td>
-                    <td className="table-data">27</td>
-                    <td className="table-data">28</td>
-                  </tr>
-                  <tr className="table-row">
-                    <td className="table-data">29</td>
-                    <td className="table-data">30</td>
-                    <td className="table-data">31</td>
-                    <td className="table-data">1</td>
-                    <td className="table-data">2</td>
-                    <td className="table-data">3</td>
-                    <td className="table-data">4</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <div className="block data">
-              <div className="data-row">
-                <label htmlFor="date" className="data-name">
-                  Дата:
-                </label>
-                <input
-                  type="text"
-                  id="date"
-                  className="data-value"
-                  placeholder="__.__.__"
-                />
-              </div>
-              <div className="data-row">
-                <label htmlFor="time" className="data-name">
-                  Время:
-                </label>
-                <input
-                  type="text"
-                  id="time"
-                  className="data-value"
-                  placeholder="00:00"
-                />
-              </div>
-            </div>
-            <button className="btn save-btn">Save</button>
-          </div>
-        </div>
       </div>
     </>
   );
--- src/components/ModalSettings/ModalSettings.jsx
+++ src/components/ModalSettings/ModalSettings.jsx
@@ -1,36 +1,121 @@
 export default function ModalSettings(props) {
-  const { data } = props;
-  const date = data.date ? data.date : null;
-  const time = data.time ? data.time : null;
+  const { task, Chevron } = props;
 
   return (
     <div
-      className="modal settings"
+      className="modal settings extended-modal"
       style={{
         position: "absolute",
-        left: `${data.position.x}px`,
-        top: `${data.position.y}px`,
+        left: `${task.timeModal.position.x}px`,
+        top: `${task.timeModal.position.y}px`,
         zIndex: 2,
-        border: "1px solid black",
+        border: "1px solid #e2e2e2",
       }}
     >
       <div className="content">
-        <div className="line">
-          <p className="property">Date:</p>
-          {date ? (
-            <p className="value">{date}</p>
-          ) : (
-            <p className="value">empty</p>
-          )}
+        <div className="block calendar">
+          <div className="period">
+            <button className="btn">
+              <Chevron rotate={-90}></Chevron>
+            </button>
+            <p className="text">Март</p>
+            <button className="btn">
+              <Chevron rotate={90}></Chevron>
+            </button>
+          </div>
+          <div className="period">
+            <button className="btn">
+              <Chevron rotate={-90}></Chevron>
+            </button>
+            <p className="text">2025</p>
+            <button className="btn">
+              <Chevron rotate={90}></Chevron>
+            </button>
+          </div>
+          <table className="calendar-table">
+            <tbody>
+              <tr className="table-row">
+                <th className="table-heading">Пн</th>
+                <th className="table-heading">Вт</th>
+                <th className="table-heading">Ср</th>
+                <th className="table-heading">Чт</th>
+                <th className="table-heading">Пт</th>
+                <th className="table-heading">Сб</th>
+                <th className="table-heading">Вс</th>
+              </tr>
+              <tr className="table-row">
+                <td className="table-data">1</td>
+                <td className="table-data">2</td>
+                <td className="table-data">3</td>
+                <td className="table-data">4</td>
+                <td className="table-data">5</td>
+                <td className="table-data">6</td>
+                <td className="table-data">7</td>
+              </tr>
+              <tr className="table-row">
+                <td className="table-data">8</td>
+                <td className="table-data">9</td>
+                <td className="table-data">10</td>
+                <td className="table-data">11</td>
+                <td className="table-data">12</td>
+                <td className="table-data">13</td>
+                <td className="table-data">14</td>
+              </tr>
+              <tr className="table-row">
+                <td className="table-data">15</td>
+                <td className="table-data">16</td>
+                <td className="table-data">17</td>
+                <td className="table-data">18</td>
+                <td className="table-data">19</td>
+                <td className="table-data">20</td>
+                <td className="table-data">21</td>
+              </tr>
+              <tr className="table-row">
+                <td className="table-data">22</td>
+                <td className="table-data">23</td>
+                <td className="table-data">24</td>
+                <td className="table-data">25</td>
+                <td className="table-data">26</td>
+                <td className="table-data">27</td>
+                <td className="table-data">28</td>
+              </tr>
+              <tr className="table-row">
+                <td className="table-data">29</td>
+                <td className="table-data">30</td>
+                <td className="table-data">31</td>
+                <td className="table-data">1</td>
+                <td className="table-data">2</td>
+                <td className="table-data">3</td>
+                <td className="table-data">4</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-        <div className="line">
-          <p className="property">Time:</p>
-          {time ? (
-            <p className="value">{time}</p>
-          ) : (
-            <p className="value">empty</p>
-          )}
+        <div className="block data">
+          <div className="data-row">
+            <label htmlFor="date" className="data-name">
+              Дата:
+            </label>
+            <input
+              type="text"
+              id="date"
+              className="data-value"
+              placeholder="__.__.__"
+            />
+          </div>
+          <div className="data-row">
+            <label htmlFor="time" className="data-name">
+              Время:
+            </label>
+            <input
+              type="text"
+              id="time"
+              className="data-value"
+              placeholder="00:00"
+            />
+          </div>
         </div>
+        <button className="btn save-btn">Save</button>
       </div>
     </div>
   );
