4eec2b8bdd32dfe857c9be28a2d5de943fe69db6
Daniil Surin <wkingsurin@gmail.com>
2025-04-11 09:54:16 +0500

    fix, refactor, Fix closing date modal on tab switch, extract 'tabs' JSX from 'App' to 'AppContainer'

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -15,55 +15,10 @@ export default function App() {
 		notification: { text: "Task saved!" },
 	}));
 
-	const switchTab = (e) => {
-		setContent((prev) => {
-			return {
-				...prev,
-				isActiveTab: e.target.name,
-			};
-		});
-	};
-
 	return (
 		<Providers>
 			<div className="app">
-				<AppContainer>
-					<div className="tabs">
-						<button
-							className={`btn ${content.isActiveTab == "new" ? "active" : ""}`}
-							name="new"
-							onClick={(e) => switchTab(e)}
-						>
-							New
-						</button>
-						<button
-							className={`btn ${
-								content.isActiveTab == "actual" ? "active" : ""
-							}`}
-							name="actual"
-							onClick={(e) => switchTab(e)}
-						>
-							Actual
-						</button>
-						<button
-							className={`btn ${
-								content.isActiveTab == "wasted" ? "active" : ""
-							}`}
-							name="wasted"
-							onClick={(e) => switchTab(e)}
-						>
-							Wasted
-						</button>
-						<button
-							className={`btn ${
-								content.isActiveTab == "completed" ? "active" : ""
-							}`}
-							name="completed"
-							onClick={(e) => switchTab(e)}
-						>
-							Completed
-						</button>
-					</div>
+				<AppContainer content={content} setContent={setContent}>
 					<TasksContainer
 						content={content}
 						setContent={setContent}
--- src/components/AppContainer/AppContainer.jsx
+++ src/components/AppContainer/AppContainer.jsx
@@ -4,12 +4,54 @@ import { useDateModal } from "../../features/dateModal/useDateModal";
 import ModalAlert from "../ModalAlert";
 import ModalSettings from "../ModalSettings";
 
-export default function AppContainer({ children }) {
-	const { dateModal } = useDateModal();
+export default function AppContainer({ content, setContent, children }) {
+	const { dateModal, handlers } = useDateModal();
 	const { alert } = useAlert();
+
+	const switchTab = (e) => {
+		setContent((prev) => {
+			return {
+				...prev,
+				isActiveTab: e.target.name,
+			};
+		});
+		handlers.hideDateModal()
+	};
 
 	return (
 		<div className="app-container">
+			<div className="tabs">
+				<button
+					className={`btn ${content.isActiveTab == "new" ? "active" : ""}`}
+					name="new"
+					onClick={(e) => switchTab(e)}
+				>
+					New
+				</button>
+				<button
+					className={`btn ${content.isActiveTab == "actual" ? "active" : ""}`}
+					name="actual"
+					onClick={(e) => switchTab(e)}
+				>
+					Actual
+				</button>
+				<button
+					className={`btn ${content.isActiveTab == "wasted" ? "active" : ""}`}
+					name="wasted"
+					onClick={(e) => switchTab(e)}
+				>
+					Wasted
+				</button>
+				<button
+					className={`btn ${
+						content.isActiveTab == "completed" ? "active" : ""
+					}`}
+					name="completed"
+					onClick={(e) => switchTab(e)}
+				>
+					Completed
+				</button>
+			</div>
 			{children}
 			{dateModal.isOpen && <ModalSettings></ModalSettings>}
 			{alert.isOpen && <ModalAlert></ModalAlert>}
